284e78d17403a50e5ab56101a87a680981bc77d2
Travis Rodriguez <travisgolembiewski@gmail.com>
2026-05-15 14:56:24 -0500

    find active session is now searching through active sessions dicstionary if plate matches or returns none

--- database.py
+++ database.py
@@ -17,11 +17,24 @@ class ParkingDatabase:
 
     ##### Create Session #####
     def create_session(self, plate, vehicle_type, driver_photo, passenger_photo, mode, entry_time):
-        
+        session = {
+            "plate": plate,
+            "vehicle_type": vehicle_type,
+            "driver_photo": driver_photo,
+            "passenger_photo": passenger_photo,
+            "mode": mode,
+            "entry_time": entry_time
+        }
+        self.active_session[plate] = session
+        return session
 
     ##### Find Active Session #####
-    def find_active_session():
-
+    def find_active_session(self, plate):
+        if plate in self.active_sessions:
+            return self.active_sessions[plate]
+        else:
+            return None
 
     ##### Close Session #####
     def close_session():
+        

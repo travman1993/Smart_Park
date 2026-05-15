91781ff87df1bae4f4f15e8788dddf7b05188d08
Travis Rodriguez <travisgolembiewski@gmail.com>
2026-05-15 14:09:25 -0500

    added needed funciton placement in database.py for main.py

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 
+# Main Class
 class ParkingDatabase:
+    ###### Initializing #####
     def __init__(self):
+        # Variables
         self.active_sessions = {}
         self.completed_session = []
         self.residential_plate ={}
@@ -9,3 +12,15 @@ class ParkingDatabase:
         self.blacklist = []
         self.security_logs = []
         self.total_revenue = 0
+
+    ##### Create Session #####
+    def create_session():
+
+
+    ##### Find Active Session #####
+    def find_active_session():
+
+
+    ##### Close Session #####
+    def close_session():
+             

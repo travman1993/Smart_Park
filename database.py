244c9db8de77296ba0fb437de9e15607f2a4777c
Travis Rodriguez <travisgolembiewski@gmail.com>
2026-05-15 15:28:43 -0500

    close session in database.py now done, slight removal of variables not needed here but will need in another file

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-import main
 
 # Main Class
 class ParkingDatabase:
@@ -8,12 +7,9 @@ class ParkingDatabase:
     def __init__(self):
         # Variables
         self.active_sessions = {}
-        self.completed_session = []
+        self.completed_sessions = []
         self.residential_plate ={}
-        self.vehivle_records = {}
-        self.blacklist = []
-        self.security_logs = []
-        self.total_revenue = 0
+        self.vehicle_records = {}
 
     ##### Create Session #####
     def create_session(self, plate, vehicle_type, driver_photo, passenger_photo, mode, entry_time):
@@ -25,7 +21,7 @@ class ParkingDatabase:
             "mode": mode,
             "entry_time": entry_time
         }
-        self.active_session[plate] = session
+        self.active_sessions[plate] = session
         return session
 
     ##### Find Active Session #####
@@ -36,5 +32,15 @@ class ParkingDatabase:
             return None
 
     ##### Close Session #####
-    def close_session():
-        
+    def close_session(self, plate, exit_time, charge):
+        if plate in self.active_sessions:
+            session = self.active_sessions[plate]
+            session["exit_time"] = exit_time
+            session["charge"] = charge
+            session["duration"] = (
+                exit_time - session["entry_time"]
+            )
+            self.completed_sessions.append(session)
+            del self.active_sessions[plate]
+            return session
+        return None

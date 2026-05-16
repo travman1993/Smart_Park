b68ec33d083bc689f43522a35536a2958648fbe9
Travis Rodriguez <travisgolembiewski@gmail.com>
2026-05-16 01:05:43 -0400

    billing Charge funciton done, mock processing payment done for now

--- billing.py
+++ billing.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 ##### Main Class #####
 class BillingEngine:
+    ##### Initializing #####
     def __init__(self):
         self.hourly_rate = 5
         self.daily_max = 25
@@ -9,8 +10,23 @@ class BillingEngine:
 
     ##### Calculate Charge #####
     def calculate_charge(self, entry_time, exit_time):
-        None    
+        duration = exit_time - entry_time
+        minutes_parked = duration.total_seconds() / 60
 
+        if minutes_parked <= self.grace_period:
+            return 0
+        
+        hours_parked = minutes_parked / 60
+        charge = hours_parked * self.hourly_rate 
+            
+        if charge > self.daily_max:
+            charge = self.daily_max
+        
+        return round(charge, 2)
+        
     ##### Process Payment #####
     def process_payment(self, charge):
-        None
+        # Mock Processing
+        print(f"Processing payment: ${charge}")
+
+        return True

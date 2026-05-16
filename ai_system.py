af3f0036d49138578aca94a51f978444152c3eb6
Travis Rodriguez <travisgolembiewski@gmail.com>
2026-05-16 11:32:08 -0400

    ai_system.py is structured , for the base setup

---
+++ ai_system.py
@@ -0,0 +1,13 @@
+##### Main Class #####
+class AIRecognition:
+    ##### Initializing #####
+    def __init__(self):
+        None
+    
+    ##### OCR Plate Check #####
+    def scan_plate(self, plate_photo):
+        None
+
+    ##### Validate Access #####
+    def validate_access(self, plate, current_mode):
+        None

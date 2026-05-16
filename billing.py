f23ac34f190b77e0d852124ce573e9a487d377ee
Travis Rodriguez <travisgolembiewski@gmail.com>
2026-05-16 00:09:34 -0400

    billing.py structure set up and function set up

---
+++ billing.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+##### Main Class #####
+class BillingEngine:
+    def __init__(self):
+        None
+
+    ##### Calculate Charge #####
+    def calculate_charge(self, session, exit_time):
+        None    
+
+    ##### Process Payment #####
+    def process_payment(self, charge):
+        None

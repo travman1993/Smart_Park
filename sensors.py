d788213c6440c6ca0529ba9f4ea1c71a25e32105
Travis Rodriguez <travisgolembiewski@gmail.com>
2026-05-17 01:06:29 -0400

    sensor.py structure layed out

---
+++ sensors.py
@@ -0,0 +1,10 @@
+##### Main Class #####
+class SensorSystem:
+    ##### Initializing #####
+    def __init__(self):
+        None
+
+    ##### Entry Triggered #####
+
+
+    ##### Exit Triggered #####

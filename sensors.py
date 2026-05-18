28d5b10995498fece66d86b131a5bae77fc38d3e
Travis Rodriguez <travisgolembiewski@gmail.com>
2026-05-17 21:34:55 -0400

    sensor.py structure established

--- sensors.py
+++ sensors.py
@@ -2,9 +2,16 @@
 class SensorSystem:
     ##### Initializing #####
     def __init__(self):
+        self.vehicle_at_entry = {}
+        self.vehicle_at_entry = {}
+        
+    ##### Entry Triggered #####
+    def entry_triggered(self):
         None
 
-    ##### Entry Triggered #####
-
-
     ##### Exit Triggered #####
+    def exit_triggered(self):
+        None
+    ##### Detect Vehicle Type #####
+    def detect_vehicle_type(self):
+        None

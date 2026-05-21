d97f9bf8137e14ef5c3bd5f3a6504d4dec361a7b
Travis Rodriguez <travisgolembiewski@gmail.com>
2026-05-21 00:25:11 -0400

    Add sensor simulation logic with random trigger probabilities

--- sensors.py
+++ sensors.py
@@ -1,17 +1,28 @@
+import random
+
 ##### Main Class #####
 class SensorSystem:
     ##### Initializing #####
     def __init__(self):
-        self.vehicle_at_entry = {}
-        self.vehicle_at_exit = {}
-        
+        self.vehicle_at_entry = False
+        self.vehicle_at_exit = False
+        self.vehicle_list = []
+
     ##### Entry Triggered #####
     def entry_triggered(self):
-        None
+        self.vehicle_at_entry = random.random() < 0.10
+        return self.vehicle_at_entry
 
     ##### Exit Triggered #####
     def exit_triggered(self):
-        None
+        self.vehicle_at_exit = random.random() < 0.05
+        return self.vehicle_at_exit
+    
     ##### Detect Vehicle Type #####
     def detect_vehicle_type(self):
-        None
+        vehicle_type = ['Motorcycle', 'Car', 'SUV', 'Truck'] 
+        random_vehicle = random.choice(vehicle_type)
+
+        self.vehicle_list.append(random_vehicle)
+
+        return random_vehicle
